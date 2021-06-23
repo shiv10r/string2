f4ae2a8a9353ed3d666b3aa566134ebc03c6718e
shiv10r <shiv99stark@gmail.com>
2021-06-23 17:41:41 +0530

    third

--- src/com/shrgfg/Main.java
+++ src/com/shrgfg/Main.java
@@ -1,10 +1,18 @@
 package com.shrgfg;
 
-import static com.shrgfg.str.isPallindrome;
+import static com.shrgfg.str.Anagram;
+//import static com.shrgfg.str.isPallindrome;
 
 public class Main {
 
     public static void main(String[] args) {
+         str  strr =  new str();
+        System.out.println(Anagram("fkvhdfdhv","fjbvjhfbv"));
+        // String s1 =  "abdfhbbis";
+         //String s2 =  "fhvbh" ;
+
+
+        /*
           str  sttr =  new str();
            isPallindrome("aaallljjj");
           // str.isPallindrome("ddfbngrjjjj");
@@ -14,6 +22,8 @@ public class Main {
             System.out.println("Yes");
         else
             isPallindrome("No");
+
+         */
 
 
 
--- src/com/shrgfg/str.java
+++ src/com/shrgfg/str.java
@@ -1,7 +1,27 @@
 package com.shrgfg;
 
+import java.util.Arrays;
+
 public class str {
+
+     static boolean  Anagram(String s1, String s2){
+          if(s1.length() !=    s2.length())
+              return  false;
+
+             char a1[] =  s1.toCharArray();
+             Arrays.sort(a1);
+
+          s1 =  new String(a1);
+          //converting string to array first
+          char a2[] =  s2.toCharArray();
+          Arrays.sort(a2);
+          s2 =  new String(a2);
+            // returning the if equal value
+          return  s1.equals(s2);
+     }
+    /*
     static boolean isPallindrome(String str) {
+
         int i = 0,   j =   str.length() -1;
 
         while( i < j){
@@ -14,4 +34,6 @@ public class str {
         return true;
 
     }
+
+     */
 }
