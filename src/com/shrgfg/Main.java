8cbd197dc5e6faf646266d7296f166eb23d47021
shiv10r <shiv99stark@gmail.com>
2021-06-23 16:15:56 +0530

    initial

--- src/com/shrgfg/Main.java
+++ src/com/shrgfg/Main.java
@@ -1,8 +1,22 @@
 package com.shrgfg;
 
+import static com.shrgfg.str.isPallindrome;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+          str  sttr =  new str();
+           isPallindrome("aaallljjj");
+          // str.isPallindrome("ddfbngrjjjj");
+
+        //String str = "gggjjjdddgg";
+        if (isPallindrome("dfjbvh"))
+            System.out.println("Yes");
+        else
+            isPallindrome("No");
+
+
 
     }
 }
+
---
+++ src/com/shrgfg/str.java
@@ -0,0 +1,17 @@
+package com.shrgfg;
+
+public class str {
+    static boolean isPallindrome(String str) {
+        int i = 0,   j =   str.length() -1;
+
+        while( i < j){
+            if(str.charAt(i) !=   str.charAt(j))
+                return false;
+            i ++;
+            j--;
+
+        }
+        return true;
+
+    }
+}
